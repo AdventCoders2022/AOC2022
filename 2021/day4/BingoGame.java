ada569fd1ea6f7e065170c67d5cfc196bbd2de7e
AdventCoders2022 <118778484+AdventCoders2022@users.noreply.github.com>
2024-04-26 23:33:18 -0600

    Update BingoGame.java

--- 2021/day4/BingoGame.java
+++ 2021/day4/BingoGame.java
@@ -11,6 +11,7 @@ public class BingoGame {
 //	static int[] nums = { 94,21,58,16,4,1,44,6,17,48,20,92,55,36,40,63,62,2,47,7,46,72,85,24,66,49,34,56,98,41,84,23,86,64,28,90,39,97,73,81,12,69,35,26,75,8,32,77,52,50,5,96,14,31,70,60,29,71,9,68,19,65,99,57,54,61,33,91,27,78,43,95,42,3,88,51,53,30,89,87,93,74,18,15,80,38,82,79,0,22,13,67,59,11,83,76,10,37,25,45};
 	static int[] nums = {7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1};
 	public static void main(String[] args) {
+		// Added the comment line to verify if my name is captured in the commiter logs
 		System.out.println("Hello World");
 
 		int inputCount = getInputCount(nums);
